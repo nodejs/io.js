d895ce179223b1a1dd162409a0418335d827b336
Vladimir Morozov <vmorozov@microsoft.com>
2025-01-30 21:14:02 -0800

    update embedding API and add C tests

--- src/api/embed_helpers.cc
+++ src/api/embed_helpers.cc
@@ -102,19 +102,7 @@ Maybe<ExitCode> SpinEventLoopInternalImpl(Environment* env) {
 
     env->PrintInfoForSnapshotIfDebug();
     env->ForEachRealm([](Realm* realm) { realm->VerifyNoStrongBaseObjects(); });
-    Maybe<ExitCode> exit_code = EmitProcessExitInternal(env);
-    if (exit_code.FromMaybe(ExitCode::kGenericUserError) !=
-        ExitCode::kNoFailure) {
-      return exit_code;
-    }
-
-    auto unsettled_tla = env->CheckUnsettledTopLevelAwait();
-    if (unsettled_tla.IsNothing()) {
-      return Nothing<ExitCode>();
-    }
-    if (!unsettled_tla.FromJust()) {
-      return Just(ExitCode::kUnsettledTopLevelAwait);
-    }
+    return EmitProcessExitInternal(env);
   }
   return Just(ExitCode::kNoFailure);
 }
